7f7a55fdeed1cf0aced42249e26753a3bdd47e80
Tyler Distad <tyler.distad@gmail.com>
2019-04-06 21:15:36 +0000

    Add a TODO caveat that this does NOT block the thread

--- sleep.js
+++ sleep.js
@@ -1,4 +1,6 @@
 module.exports = {
+  // TODO: calling this as shown does NOT actually block the thread; figure out why
+  //  .then(async () => await sleep(3000))
   sleep: function sleep(ms) {
     // inspired by https://stackoverflow.com/a/39914235/421313
     return new Promise(resolve => setTimeout(resolve, ms))
